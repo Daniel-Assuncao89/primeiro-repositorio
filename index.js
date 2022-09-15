379281892a5d6923f388f13ada1e2273f02689a8
Daniel-Assuncao89 <assuncao.daniel89@gmail.com>
2022-09-15 17:52:26 -0300

    Adicionando HTML e JS

---
+++ index.js
@@ -0,0 +1,16 @@
+/**
+ * -> Tipos primitivos do JavaScript string, number, boolean
+ * -> variaveis no Javascript
+ * -> Operadores matematicos do Javascript
+ */
+
+console.log("Hello World")
+console.log(5)
+console.log(43.76)
+console.log(true)
+
+//variaveis
+let mensagem = "Minha mensagem"
+console.log(mensagem)
+mensagem = "Novo texto SoulCode BC 25 JFS"
+console.log(mensagem)
