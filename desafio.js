98afceda8a0312d57b0e0062c35434a9984a62de
Daniel-Assuncao89 <assuncao.daniel89@gmail.com>
2022-09-21 17:26:05 -0300

    Desafio Combustivel e desafio aula

---
+++ desafio.js
@@ -0,0 +1,54 @@
+/**
+ * Elabore um algoritmo que calcule o que deve ser pago por um produto, considerando o preço
+normal de etiqueta e a escolha da condição de pagamento. Utilize os códigos da tabela a seguir
+para ler qual a condição de pagamento escolhida e efetuar o cálculo adequado.
+Código Condição de pagamento
+
+1 À vista em dinheiro ou cheque, recebe 10% de desconto
+2 À vista no cartão de crédito, recebe 15% de desconto
+3 Em duas vezes, preço normal de etiqueta sem juros
+4 A partir de três vezes, preço normal de etiqueta mais juros de 10%
+ */
+
+/**
+ * -> Metodos de pagamento
+ * *Em dinheiro
+ * *Em cheque
+ * *No Cartão
+ */
+
+const precoProduto = parseFloat(prompt('Qual o preço do produto?'))
+const metodoDePagamento = prompt(`
+Informe o método de pagamento:
+Digite 1 para pagamento em dinheiro
+Digite 2 para pagamento em cheque
+Digite 3 para pagamento com cartão
+`)
+let precoFinal = 0
+
+console.log(precoProduto)
+console.log(metodoDePagamento)
+
+if (metodoDePagamento == 1 || metodoDePagamento == 2) {
+
+    precoFinal = precoProduto * 0.9
+    alert(`O preço final do pagamento é ${precoFinal.toFixed(2)}`)
+
+}else if(metodoDePagamento == 3){
+    const quantidadeDeParcelas = parseInt(prompt(`Informe a quantidade de parcelas`))
+    if(quantidadeDeParcelas == 1){
+        precoFinal = precoProduto * 0.85
+        alert(`O preço final do pagamento é ${precoFinal.toFixed(2)}`)
+    }else if(quantidadeDeParcelas == 2){
+        precoFinal = precoProduto
+        alert(`O preço final do pagamento é ${precoFinal.toFixed(2)}`)
+    }else if(quantidadeDeParcelas >= 3 && quantidadeDeParcelas <= 10){
+        precoFinal = precoProduto * 1.10
+        alert(`O preço final do pagamento é ${precoFinal.toFixed(2)}`)
+    }else {
+        alert('Quantidade de parcelas invalida')
+    }
+
+}else {
+    alert('Numero invalido')
+}
---
+++ desafioCombustivel/desafioCombustivel.js
@@ -0,0 +1,27 @@
+const tipoDeCombustivel = prompt(`
+Selecione qual o tipo de Combustivel
+1) Alcool
+2) Gasolina`)
+let valorFinal = 0
+
+if(tipoDeCombustivel == 1){
+    const alcool = parseFloat(prompt('Quantidade de Litros de alcool?'))
+    if (alcool <= 25){
+        valorFinal = (alcool * 1.90) * 0.98
+        alert(`Valor a Pagar: ${valorFinal}`)
+    } else {
+        valorFinal = (alcool * 1.90) * 10.96
+        alert(`Valor a Pagar: ${valorFinal}`)
+    }
+}else if (tipoDeCombustivel == 2){
+    const gasolina = parseFloat(prompt('Quantidade de Litros de Gasolina?'))
+    if (gasolina <= 25){
+        valorFinal = (gasolina * 2.70) * 0.97
+        alert(`Valor a Pagar: ${valorFinal}`)
+    }else {
+        valorFinal = (gasolina * 2.70) * 0.95
+        alert(`Valor a Pagar: ${valorFinal}`)
+    }
+}else {
+    alert('Opção Invalida')
+}
